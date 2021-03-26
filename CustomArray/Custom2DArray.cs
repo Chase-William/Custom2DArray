cfbc08917847a748c1e7a0d693c303ca04370553
Chase R <46757278+ChaseRoth@users.noreply.github.com>
2021-03-26 15:57:19 -0400

    initial commit

---
+++ CustomArray/Custom2DArray.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace CustomArray
+{
+    /// <summary>
+    /// A <see cref="Custom2DArray{T}"/> that acts like a 2d array but really is just a 1d array under the hood.
+    /// </summary>
+    /// <typeparam name="T">Whatever you want bud.</typeparam>
+    public class Custom2DArray<T>
+    {
+        /// <summary>
+        /// The underling 1d array.
+        /// </summary>
+        private T[] UnderlyingArray { get; set; }
+        /// <summary>
+        /// Gets the Length of the underlying array.
+        /// </summary>
+        public int Length => UnderlyingArray.Length;
+        /// <summary>
+        /// Gets the column count assigned in the constructor.
+        /// </summary>
+        public uint ColumnCount { get; private set; }
+        /// <summary>
+        /// Gets the row count assigned in teh constructor.
+        /// </summary>
+        public uint RowCount { get; private set; }
+        /// <summary>
+        /// Initializes a new <see cref="Custom2DArray{T}"/> instance.
+        /// </summary>
+        /// <param name="_rowCount"></param>
+        /// <param name="_columnCount"></param>
+        public Custom2DArray(uint _rowCount, uint _columnCount)
+        {
+            RowCount = _rowCount;
+            ColumnCount = _columnCount;
+            UnderlyingArray = new T[_rowCount * _columnCount];
+        }
+        /// <summary>
+        /// Sets the given value at the specified location. Will throw an exception if either of the provided index values are out of bounds.
+        /// </summary>
+        /// <param name="row">Row in array.</param>
+        /// <param name="column">Column in array.</param>
+        /// <param name="value">Value to be assigned.</param>
+        public void SetAt(uint row, uint column, T value)
+            => UnderlyingArray[row * ColumnCount + column] = value;
+        /// <summary>
+        /// Gets the <see cref="T"/> residing at the specified <paramref name="row"/> and <paramref name="column"/>.
+        /// </summary>
+        /// <param name="row">The row to target.</param>
+        /// <param name="column">The column to target.s</param>
+        /// <returns></returns>
+        public T GetAt(uint row, uint column)
+            => UnderlyingArray[row * ColumnCount + column];
+    }
+}
---
+++ TestThatCode/CustomArrayTest.cs
@@ -0,0 +1,45 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CustomArray;
+
+namespace TestThatCode
+{
+    [TestClass]
+    public class CustomArrayTest
+    {
+        /// <summary>
+        /// Constants used for test.
+        /// </summary>
+        const byte ROW_COUNT = 10, COLUMN_COUNT = 5;
+        [TestMethod("Smoke - Length Test")]        
+        public void LengthTest()
+        {   
+            // Standard C# 2d array
+            int[,] standard = new int[ROW_COUNT, COLUMN_COUNT];
+            // Custom 2d array type
+            var custom = new Custom2DArray<byte>(ROW_COUNT, COLUMN_COUNT);
+            // Comparing the lengths to make sure initializes works as expected.
+            Assert.AreEqual(standard.Length, custom.Length, "Error a standard lib 2d array length didn't match the custom's length with the same given values.");
+        }
+
+        [TestMethod("Smoke - Get/Set Test")]
+        [Description("Test the getter of the custom function and compares it to the standard lib implementation's result.")]
+        public void GetIndexTest()
+        {
+            const byte TARGET_ROW = 2, TARGET_COLUMN = 4; // Where we will be inserting in the test
+            const byte VALUE = 66; // The value to be inserted for the test
+
+            // Standard C# 2d array
+            int[,] standard = new int[ROW_COUNT, COLUMN_COUNT];
+            // Custom 2d array type
+            var custom = new Custom2DArray<byte>(ROW_COUNT, COLUMN_COUNT);
+
+            // Assigning value at specified index
+            standard[TARGET_ROW, TARGET_COLUMN] = VALUE;
+            custom.SetAt(TARGET_ROW, TARGET_COLUMN, VALUE);
+
+            // Comparing the result of our SetAt and GetAt methods
+            Assert.AreEqual(standard[TARGET_ROW, TARGET_COLUMN], custom.GetAt(TARGET_ROW, TARGET_COLUMN));
+        }
+    }
+}
